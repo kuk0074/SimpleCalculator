43a15d0c065269084b2b9fe2f50d694118041d70
Mateusz Kuczynski <mkuczynski0074@gmail.com>
2019-03-24 16:30:36 +0100

    Added Division Class and Multiplication Class

--- src/com/calculator/Division.java
+++ src/com/calculator/Division.java
@@ -1,4 +1,23 @@
 package com.calculator;
 
-public class Division {
+public class Division implements Actionable
+{
+    private double result;
+
+    public Division() {
+        this.result = result;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void result(double a, double b){
+        result = a / b;
+        if (b == 0) {
+            System.out.println("Cannot divide by 0");
+        } else {
+            System.out.println(a + " / " + b + " = " + result);
+        }
+    }
 }
--- src/com/calculator/Multiplication.java
+++ src/com/calculator/Multiplication.java
@@ -1,4 +1,18 @@
 package com.calculator;
 
-public class multiplication {
+public class Multiplication implements Actionable {
+ private double result;
+
+    public Multiplication() {
+        this.result = result;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void result(double a, double b){
+        this.result = a * b;
+        System.out.println(a + " * " + b + " = " + this.result);
+    }
 }
