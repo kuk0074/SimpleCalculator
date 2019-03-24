45b5a7ac404ff91114fd09e44fa7d486c2de1cc1
Mateusz Kuczynski <mkuczynski0074@gmail.com>
2019-03-24 19:59:15 +0100

    Updated Calculator Main Class

--- src/com/calculator/Calculator.java
+++ src/com/calculator/Calculator.java
@@ -9,16 +9,52 @@ public class Calculator {
     public static void main(String[] args) {
         boolean menu = true;
         int choice;
+
         while (menu) {
-            homemadeCalculator();
+            menuCalculator();
             System.out.println();
-            System.out.println("Select operation: ");
-            choice = sc.nextInt();
-            sc.nextLine();
+            do {
+                System.out.println("Select operation: ");
+                choice = -1;
+                if (!sc.hasNextInt()) {
+                    System.out.println("Please enter valid number from 1 do 5");
+                } else {
+                    choice = sc.nextInt();
+                    if (choice < 1 || choice > 5) {
+                        System.out.println(choice + " is not valid option.");
+                    }
+                }
+                sc.nextLine();
+            } while (choice < 1 || choice > 5);
+
             switch (choice) {
                 case 1:
-                    division();
+                    try {
+                        division();
+                    } catch (InputMismatchException e) {
+                        System.err.println("Wrong input. Please enter valid value(s).");
+                    }
                     break;
+                case 2:
+                    try {
+                        multiplication();
+                    } catch (InputMismatchException e) {
+                        System.err.println("Wrong input. Please enter valid value(s).");
+                    }
+                    break;
+                case 3:
+                    try {
+                        sum();
+                    } catch (InputMismatchException e) {
+                        System.err.println("Wrong input. Please enter valid value(s).");
+                    }
+                    break;
+                case 4:
+                    try {
+                        substraction();
+                    } catch (InputMismatchException e) {
+                        System.err.println("Wrong input. Please enter valid value(s).");
+                    }
                 case 5:
                     System.out.println("Shutting down calculator");
                     menu = false;
@@ -27,31 +63,63 @@ public class Calculator {
         }
     }
 
-    public static void homemadeCalculator() {
+    public static void menuCalculator() {
+        System.out.println("Press: \n 1. to divide \n 2. to multiplicate \n 3. to sum \n 4. to substract \n 5. to quit");
+    }
 
-        System.out.println("Press to go: \n 1. to divide \n 2. to multiplicate \n 3. to sum \n 4. to substract \n 5. to quit");
+    public static void multiplication() {
+        Multiplication multiplication = new Multiplication();
+        double firstNo;
+        double secondNo;
+
+        System.out.println("Enter first number:");
+        firstNo = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Enter second number:");
+        secondNo = sc.nextDouble();
+        sc.nextLine();
+        multiplication.result(firstNo, secondNo);
     }
 
     public static void division() {
+        Division division = new Division();
         double firstNo;
         double secondNo;
-        double result;
 
-        try {
-            System.out.println("Enter first number:");
-            firstNo = sc.nextDouble();
-            sc.nextLine();
-            System.out.println("Enter second number:");
-            secondNo = sc.nextDouble();
-            sc.nextLine();
-            result = firstNo / secondNo;
-            if (secondNo == 0) {
-                System.out.println("Cannot divide by 0");
-            } else {
-                System.out.println(firstNo + " / " + secondNo + " = " + result);
-            }
-        } catch (InputMismatchException ie) {
-            System.out.println("Wrong input");
-        }
+        System.out.println("Enter first number:");
+        firstNo = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Enter second number:");
+        secondNo = sc.nextDouble();
+        sc.nextLine();
+        division.result(firstNo, secondNo);
+    }
+
+    public static void sum() {
+        Sum sum = new Sum();
+        double firstNo;
+        double secondNo;
+
+        System.out.println("Enter first number:");
+        firstNo = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Enter second number:");
+        secondNo = sc.nextDouble();
+        sc.nextLine();
+        sum.result(firstNo, secondNo);
+    }
+
+    public static void substraction() {
+        Substraction substraction = new Substraction();
+        double firstNo;
+        double secondNo;
+
+        System.out.println("Enter first number:");
+        firstNo = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Enter second number:");
+        secondNo = sc.nextDouble();
+        sc.nextLine();
+        substraction.result(firstNo, secondNo);
     }
 }
