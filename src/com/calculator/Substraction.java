36674119a3d58487b33e2fa0ca61c7d39d098fa5
Mateusz Kuczynski <mkuczynski0074@gmail.com>
2019-03-24 18:30:59 +0100

    Added Substraction Class

--- src/com/calculator/Substraction.java
+++ src/com/calculator/Substraction.java
@@ -1,4 +1,20 @@
 package com.calculator;
 
-public class Substraction {
+public class Substraction implements Actionable
+{
+    private double result;
+
+    public Substraction() {
+        this.result = result;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    @Override
+    public void result(double a, double b) {
+        this.result = a - b;
+        System.out.println(a + " - " + b + " = " + this.result);
+    }
 }
